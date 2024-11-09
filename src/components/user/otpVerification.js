c1280f25b92c652bf7f535de09f1296ef6881850
Swati <Swatisharmass654@gmail.com>
2024-11-09 16:47:49 +0530

    first commit

---
+++ src/components/user/otpVerification.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function otpVerification() {
+  return (
+    <div>otpVerification</div>
+  )
+}
---
+++ src/components/user/profileManagement.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function profileManagement() {
+  return (
+    <div>profileManagement</div>
+  )
+}
